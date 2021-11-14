208a966ff994c85d1820bbb1944b137b62f38205
Ronnie G. Chagas <dev.ronniegchagas@gmail.com>
2021-11-13 22:38:58 -0300

    Setting env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'qwerty',
-      database: 'postgres',
-      entities: [],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_POST', 5432)),
+        username: configService.get('DB_USERNAME', 'postgres'),
+        password: configService.get('DB_PASSWORD', 'qwerty'),
+        database: configService.get('DB_DATABASE', 'postgres'),
+        entities: [],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [],
